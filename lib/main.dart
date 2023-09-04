7b09388943e434816a67ffacedf5ecb606632d2f
Tigist Wondimneh <wondimnehtigist@gmail.com>
2023-09-04 12:19:05 +0300

    testing CI/CD workflow

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,7 @@ class MyApp extends StatelessWidget {
         // This is the theme of your application.
         //
         // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
+        // t to start the app).
         //
         // Notice that the counter didn't reset back to zero; the application
         // state is not lost during the reload. To reset the state, use hot
